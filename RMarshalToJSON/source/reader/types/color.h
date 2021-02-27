d7f2e3473bd3f08fd56921037d835687ce1c0b77
GamingLiamStudios <swimmingliam@gmail.com>
2021-02-27 11:13:43 +1100

    Branched RMarshalToJSON to its own repo(https://github.com/GamingLiamStudios/rxdataToJSON)

--- RMarshalToJSON/source/reader/types/color.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Color
-{
-    double red;
-    double green;
-    double blue;
-    double alpha;
-};
--- RMarshalToJSON/source/reader/types/tone.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Tone
-{
-    double red;
-    double green;
-    double blue;
-    double grey;
-};
